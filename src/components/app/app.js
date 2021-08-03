e9849462d8bdca492df1bd1c04fa00e699999cc4
Artem <artem.xolod.90@gmail.com>
2021-08-03 21:34:48 +0300

    настройка маршрутизации и компонента сервиса

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import { Route, Switch } from 'react-router-dom';
+import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc/with-resto-service';
 
 import Background from './food-bg.jpg';
 
-const App = ({RestoService}) => {
-    console.log(RestoService.getMenuItems());
+const App = ({ RestoService }) => {
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+        <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
+            <AppHeader total={50} />
+            <Switch>
+                <Route exact path='/'>
+                    <MainPage />
+                </Route>
+                <Route exact path='/cart/'>
+                    <CartPage />
+                </Route>
+            </Switch>
         </div>
     )
 }
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,29 @@
 export default class RestoService {
-    getMenuItems(){
-        return []
+    _apiBase = 'http://localhost:3000';
+
+    getResource = async (url) => {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+
+        return await res.json();
     }
+
+    getMenuItems = async () => {
+        const res = await this.getResource('/menu');
+        return res.map(this._transformMenuItems);
+    }
+
+    _transformMenuItems = (menu) => {
+        return {
+            title: menu.title,
+            price: menu.price,
+            url: menu.url,
+            category: menu.category,
+            id: menu.id
+        }
+    }
+
 }
